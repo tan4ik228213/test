fd05c5f763bcb31a4be864aac83d818dde532a1d
Al <alexrogov947@gmail.com>
2024-03-13 22:38:21 +0300

    321

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,17 +1,24 @@
 import java.util.Scanner;
 public class main {
-    public static void main(String[] args) {         // G.O.V.N.O 2
+    static void clear () {
+        for (int k = 1; k < 25; k++) {
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {               // G.O.V.N.O 2
         util util = new util();
+
         String aghanim = "Аганим";
         String lens = "Линза";
         String blink = "Блинк";
 
-        System.out.println("Сейчас будет краткий курс как проиграть в G.O.V.N.O 2");            // Для Сани
+        System.out.println("Сейчас будет краткий курс как выйграть в G.O.V.N.O 2");            // Для Сани
 
         String firstHero = util.inputString("Выберете героя: Аппарат или Лион - ");
 try {
         if (firstHero.equals("Аппарат")) {
             System.out.println("Вы выбрали героя: " + firstHero);
+            clear();
             int pos = util.inputInt("Какую позицию вы собираетесь выбрать?", 1,5 );
             if (pos == 4) {
                 System.out.println("Успех,правильный выбор!");
@@ -92,8 +99,6 @@ try {
     }
 
     }
-
-
 
 
 
