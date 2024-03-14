45c487a92a801058a8bc50a04733f43ac3824c32
Al <alexrogov947@gmail.com>
2024-03-14 18:43:04 +0300

    final

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -22,19 +22,23 @@ public class main {
                 System.out.println("Вы выбрали героя: " + firstHero);
                 clear();
                 int pos = util.inputInt("Какую позицию вы собираетесь выбрать?", 1, 5);
+                clear();
                 if (pos == 4) {
                     System.out.println("Успех,правильный выбор!");
+                    clear();
                 } else {
                     System.out.println("Поздравляю, ты якорь, у тебя получилось проиграть!");
                     System.exit(1);
                 }
                 String firstItem = util.inputString("Что мы купим первым предметом(помимо сапога) (На выбор: Аганим, Линза, Блинк, Урна )? - ");
                 String urnOfShadow = "Урна";
+                clear();
                 if (firstItem.equals("Линза")) {
                     System.out.println("Удивительно, ты выбрал правильно! ");
-
+                    clear();
                 } else if (firstItem.equals("Урна")) {
                     System.out.println("Опаньки,научились думать головой? ");
+                    clear();
                 } else if (firstItem.equals("Блинк")) {
                     System.out.println("Ваш интелект примерно равен табуретке ");
                     System.exit(1);
@@ -50,6 +54,7 @@ public class main {
                 System.out.println("B(eng) - фидим");
                 System.out.println("C(eng) - размениваем свое лицо,чтобы кор фармил");
                 String firstAction = util.inputString(" ");
+                clear();
                 if (firstAction.equals("A")) {
                     System.out.println("Идем нахер и удаляем игру");
                     System.exit(1);
@@ -62,6 +67,7 @@ public class main {
                     System.out.println("А(eng) - отдадим 10 фрагов врагам");
                     System.out.println("B(eng) - Аккуратно разменивать ресурсы и просто перетягивать на себя внимание");
                     String checkBrain = util.inputString(" ");
+                    clear();
                     if (checkBrain.equals("A")) {
                         System.out.println("Отправляемся в помойку");
                         for (int i = 0; i < 25; i++) {
@@ -71,6 +77,7 @@ public class main {
                         System.exit(1);
                     } else if (checkBrain.equals("B")) {
                         System.out.println("Умничка,сложный выбор был...");
+                        clear();
                     } else {
                         System.out.println("Тупой что ли? Не видно,что выбрать надо? Отправляемся к Ленину");
                         System.exit(1);
@@ -81,6 +88,7 @@ public class main {
                 System.out.println("A(eng) - Купить смока, и пойти с командой на кора");
                 System.out.println("B(eng) - Подумать,стоит ли это делать?");
                 String move = util.inputString(" ");
+                clear();
 
                 if (move.equals("A")) {
                     System.out.println("Победа! Выйграли! Ура...");
@@ -95,9 +103,69 @@ public class main {
             }
         } catch (Exception e) {
             System.out.println("За тупость в помойку");
-            System.exit(0);                                        // TODO
+            System.exit(0);
         }
+        try {
+        if (firstHero.equals("Лион")) {
+            System.out.println("Вы выбрали героя: " + firstHero);
+            int pos = util.inputInt("Какую пазицию выюираем?", 1, 5);
+            if (pos == 4) {
+                System.out.println("Молорик,начало правильное");
+            } else {
+                System.out.println("Отправляемся в помойку");
+                System.exit(0);
 
+            }
+            String lionZatar = util.inputString("Что мы купим первой шмоткой?(после сапога). На выбор: Аганим, Линза, Блинк, Глимер");
+            String glimer = ("Глимер");
+
+            if (lionZatar.equals("Глимер")) {
+                System.out.println("Супер,начинаем думать по немногу головой");
+            } else if (lionZatar.equals("Линза")) {
+                System.out.println("Красава, идем дальше ");
+            } else if (lionZatar.equals("Блинк")) {
+                System.out.println("Вся команда в восторге от твоего решения!");
+            } else if (lionZatar.equals("Аганим")) {
+                System.out.println("Начинаем процесс удаления твоей херни в голове... ERROR 404(Not found brain) initialization...");
+            } else {
+                System.out.println("Начинаем процесс удаления твоей херни в голове... ERROR 404(Not found brain) initialization...");
+            }
+            System.out.println("Что мы будет делать на линии?");
+            System.out.println("A(eng) - стоять AFK");
+            System.out.println("B(eng) - разменивать свое лицо,чтобы дать пространство кору ");
+            System.out.println("C(eng) - умрем 10 раз вражескому кору");
+            String lionAction = util.inputString(" ");
+            if (lionAction.equals("A")) {
+                System.out.println("Идем нахер");
+                System.exit(0);
+            } else if (lionAction.equals("B")) {
+                System.out.println("Красава");
+            } else if (lionAction.equals("C")) {
+                System.out.println("В помойку отправляемся");
+                System.exit(0);
+            } else {
+                System.out.println("За тупость в помойку");
+                System.exit(0);
+            }
+            System.out.println("Идем ставить вард,видим след. ситуацию: кор качает лайн крипов,наши действия?");
+            System.out.println("A(eng) - Поставить недалеко вард и стоять не показываться,страховать кора");
+            System.out.println("B(eng) - Вебать спелом пачку и уйти");
+            String lionMove = util.inputString("");
+            if (lionMove.equals("A")) {
+                System.out.println("Красава,мы победили! держи подарок!");
+            } else if (lionMove.equals("B")) {
+                System.out.println("Удаляем игру...");
+                System.exit(0);
+            } else {
+                System.out.println("Удаляем игру...");
+                System.exit(0);
+            }
+        }
+        }catch (Exception e) {
+            System.out.println("За тупость в помойку");
+            System.exit(0);
+        }
+        System.out.println("Если ты видишь это сообщение,значит еще не все потерянно, твой подарок ожидает тебя по ссылке в дискорде");
     }
 
 }
